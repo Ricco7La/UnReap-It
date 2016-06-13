ac74795ce769b7e4b3efbcda4c0146a67985ab91
Kweena <michael.kweena.fossoul@gmail.com>
2016-06-13 16:17:41 +0200

    add Story Scene

--- Launcher.js
+++ Launcher.js
@@ -6,6 +6,7 @@
 			Phaser.AUTO, 
 			Application.name
 		);
+	Application.Game = Game;
 
 	/*
 		le premier argument est le nom de l'état,
@@ -15,6 +16,7 @@
 	Game.state.add("Preload", Application.Preload);
 	Game.state.add("LevelTest", Application.LevelTest);
 	Game.state.add("Title", Application.Title);
+	Game.state.add("Story", Application.Story);
 
 	// lancer l'ecran de lancement du jeu
 	Game.state.start("Splash");
