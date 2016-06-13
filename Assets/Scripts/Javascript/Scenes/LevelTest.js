1d7376539892e8fdae76b8f4f4ac75232aa22d27
Ricco7La <emeric.beauchot@gmail.com>
2016-06-13 11:25:28 +0200

    Map Generator End

--- Assets/Scripts/Javascript/Scenes/LevelTest.js
+++ Assets/Scripts/Javascript/Scenes/LevelTest.js
@@ -7,8 +7,17 @@ Application.LevelTest.prototype = {
 		console.log('Game Screen');
 		this.game.physics.startSystem(Phaser.Physics.P2JS);
 
-		var Map = GenerateMap('LevelTest');
+		this.Map = {};
 
+		var layers = [
+			{layerName: "BackGround", worldSize: true, collide: false },
+			{layerName: "Wall", collide: false },
+			{layerName: "Road" },
+			{layerName: "Object" }
+		];
+		var MapLayers = GenerateMap(this.game, this.Map, 'LevelTest', 'map', 'Tiles', layers);
+
+		MapLayers["Wall"].debug = true;
 	},
 	update : function(){
 
--- Assets/Scripts/Javascript/Tools/MapGenerator.js
+++ Assets/Scripts/Javascript/Tools/MapGenerator.js
@@ -1,26 +1,96 @@
 /*
 *
+* a MapGenerator to use with .json files from Tiled Editor
+* 
 *
+* Parameters :
+* --------------
+* _Game: reference to the game
+* _Map : reference to the level map
+* _tilemap: .json file 
+* _tilesetName: name of the tileset in Tiled Editor
+* _tilesetFile: .png file
+* _tilesLayers: array of litterals objects 
+*							=> layerName: name of the layer in Tiled Editor
+*							=> worldSize: true if the layer is the base of the world
+*							=> collide: true if the layer have to collide with other object
 *
+* Return :
+* ----------
+* a litteral object with the layer + an array the Ennemies + an array the Souls + an array of Items(switch)
 *
 */
-function GenerateMap(_tilemap, _tilesetName, _tilesetFile, tilesLayers ) 
+function GenerateMap(_Game, _Map, _tilemap, _tilesetName, _tilesetFile, _tilesLayers ) 
 {
-	this.map = this.game.add.tilemap(_tilemap,);
-	this.map.addTilesetImage(_tilesetName, _tilesetFile);
+	console.log(_Game);
+	_Map = _Game.add.tilemap(_tilemap);
+	_Map.addTilesetImage(_tilesetName, _tilesetFile);
 	
-	console.log(this.map);
-	this.layerBackGround = this.map.createLayer("BackGround");
-	this.layerBackGround.resizeWorld();
-	this.layerWall = this.map.createLayer("Wall");
-	this.layerRoad = this.map.createLayer("Road");
-	this.layerObject = this.map.createLayer("Object");
+	console.log("Map");
+	console.log(_Map);
+
+	/***** Charge Tile Layer from Tiled *****/
+	var Layers = {};
+	for (prop of _tilesLayers) 
+	{
+		Layers[prop.layerName] = _Map.createLayer(prop.layerName);
+		if (prop.worldSize) 
+		{
+			Layers[prop.layerName].resizeWorld();
+			_Game.physics.p2.setBoundsToWorld(true, true, true, true, false);
+		}
+		if (prop.collide) 
+		{
+			Layers[prop.layerName].enableBody = true;
+		}
+	}
+
+	/***** Charge Object Layer from Tiled *****/
 	
-	this.game.physics.p2.setBoundsToWorld(true, true, true, true, false);
-	this.layerWall.debug = true;
-	this.layerWall.enableBody = true;
+	/* Player Start */
+	console.log("StartPosition");
+	//console.log(_Map.objects.StartPosition[0]);
+	var StartPosition = _Map.objects.StartPosition[0];
+	console.dir(StartPosition);
+
+	/* Ennemies */
+	console.log("Ennemies");
+	//console.log(_Map.objects.Ennemies);
+	var EnnemiesPaths = [];
+	for (el of _Map.objects.Ennemies) 
+	{
+		var array = el.name.split('-');
+		var ennemiIndex = array[0];
+		var pathIndex = array[1];
+		if (EnnemiesPaths[ennemiIndex] == undefined) 
+		{
+			EnnemiesPaths[ennemiIndex] = []
+		}
+		EnnemiesPaths[ennemiIndex][pathIndex] = el;
+	}
+	console.dir(EnnemiesPaths);
+
+	/* Souls */
+	console.log("Souls");
+	var SoulsPositions = _Map.objects.Souls
+	console.dir(SoulsPositions);
+
+	/* Interractions */
+	console.log("Interractions");
+	var Interractions = [];
+	for (el of _Map.objects.Interractions) 
+	{
+		var array = el.name.split('-');
+		var type = array[0];
+		var index = array[1];
+		if (Interractions[index] == undefined) 
+		{
+			Interractions[index] = []
+		}
+		Interractions[index].push(el);
+	}
+	console.log(Interractions);
 	
-	//this.layerEnnemiesWalker = this.map.createLayer("EnnemiesWalker");
-	console.log(this.map.objects.EnnemiesWalker);
-	//this.map.setCollisionBetween(0,11,true,this.layerWall);
+
+	return Layers;
 }
