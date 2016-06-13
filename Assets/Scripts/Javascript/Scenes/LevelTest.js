c8c3d982d35a4086a24647fb879e32058353d2de
Ricco7La <emeric.beauchot@gmail.com>
2016-06-13 10:02:27 +0200

    Map Generator start

--- Assets/Scripts/Javascript/Scenes/LevelTest.js
+++ Assets/Scripts/Javascript/Scenes/LevelTest.js
@@ -5,30 +5,10 @@ Application.LevelTest = function(){}
 Application.LevelTest.prototype = {
 	create: function(){ 
 		console.log('Game Screen');
-		//this.game.physics.startSystem(Phaser.Physics.P2JS);
+		this.game.physics.startSystem(Phaser.Physics.P2JS);
 
-		this.game.physics.startSystem(Phaser.Physics.ARCADE);
+		var Map = GenerateMap('LevelTest');
 
-		this.map = this.game.add.tilemap('LevelTest');
-		this.map.addTilesetImage('map','Tiles');
-		
-		console.log(this.map);
-
-		this.layerBackGround = this.map.createLayer("BackGround");
-		this.layerWall = this.map.createLayer("Wall");
-		this.layerRoad = this.map.createLayer("Road");
-		this.layerObject = this.map.createLayer("Object");
-		
-		this.game.physics.enable(this.layerWall, Phaser.Physics.ARCADE);
-		this.layerWall.debug = true;
-		this.layerWall.enableBody = true;
-
-		this.layerBackGround.resizeWorld();
-
-		//this.layerEnnemiesWalker = this.map.createLayer("EnnemiesWalker");
-		console.log(this.map.objects.EnnemiesWalker);
-
-		//this.map.setCollisionBetween(0,11,true,this.layerWall);
 	},
 	update : function(){
 
