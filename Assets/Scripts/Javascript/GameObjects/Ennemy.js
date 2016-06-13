d75c39af847ba51bfadafdd126caaf73fc56663d
Ricco7La <emeric.beauchot@gmail.com>
2016-06-13 16:08:54 +0200

    Ennemy fix rotation

--- Assets/Scripts/Javascript/GameObjects/Ennemy.js
+++ Assets/Scripts/Javascript/GameObjects/Ennemy.js
@@ -8,7 +8,10 @@ function Ennemy (game, path, type) {
     _self.path = path;
     game.physics.p2.enable(_self);
 
-    _self.animations.add('move');
+    _self.animations.add('move_down', [0,1,2,3]);
+    _self.animations.add('move_left', [4,5,6,7]);
+    _self.animations.add('move_right', [8,9,10,11]);
+    _self.animations.add('move_up', [12,13,14,15]);
     
     _self.body.setZeroVelocity();
     //_self.body.damping= 0;
@@ -17,7 +20,7 @@ function Ennemy (game, path, type) {
 
     _self.body.collideWorldBounds = true;
 
-    _self.animations.play('move', 10, true);
+    _self.animations.play('move_down', 10, true);
 
     _self.position.x = 60;
 
