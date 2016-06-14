aed02281dae8d96fea43665d8889c0cee97a548a
Ricco7La <emeric.beauchot@gmail.com>
2016-06-14 12:04:29 +0200

    CollisionGroup

--- Assets/Scripts/Javascript/Scenes/LevelTest.js
+++ Assets/Scripts/Javascript/Scenes/LevelTest.js
@@ -22,7 +22,7 @@ Application.LevelTest.prototype = {
 		var MapLayers = GenerateMap(this.game, this.Map, 'LevelTest', 'All_Tiles', 'Tiles', layers);
 
 		//console.log(Application);
-		this.E1 = new Ennemy(this.game,[{x: 150, y:150},{x: 350, y:150},{x: 350, y:350},{x: 150, y:350}]);
+		//this.E1 = new Ennemy(this.game,[{x: 150, y:150},{x: 350, y:150},{x: 350, y:350},{x: 150, y:350}]);
 		//this.E1 = new Ennemy(this.game,[{x: 150, y:150, rotation: 0},{x: 150, y:150, rotation: 90},{x: 150, y:150, rotation: 180},{x: 150, y:150, rotation: 270}]);
 
 		this.game.camera.follow(this.E1);
