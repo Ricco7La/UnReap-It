aba31ab8011db1eac3a54503fcfc236aea15dad3
Ricco7La <emeric.beauchot@gmail.com>
2016-06-16 13:12:12 +0200

    Exit fix position

--- Assets/Scripts/Javascript/GameObjects/Out.js
+++ Assets/Scripts/Javascript/GameObjects/Out.js
@@ -3,8 +3,8 @@ function Out(game,x,y,w,h)
 
     var _self = game.add.sprite(x, y, "");
     game.physics.p2.enable(_self);
-        _self.body.x = x;
-        _self.body.y = y;
+        _self.body.x = x + w * 0.5;
+        _self.body.y = y + h * 0.5;
         _self.body.setRectangle(w,h,10,20,0);
 
         _self.body.fixedRotation = true;
