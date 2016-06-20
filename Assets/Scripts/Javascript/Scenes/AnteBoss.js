4d33b9b6ac0ad55cb05cd5b17164e0b0609a4d47
Kweena <michael.kweena.fossoul@gmail.com>
2016-06-20 14:24:58 +0200

    add Boss Dialogue

--- Assets/Scripts/Javascript/Scenes/AnteBoss.js
+++ Assets/Scripts/Javascript/Scenes/AnteBoss.js
@@ -16,35 +16,47 @@ Application.AnteBoss.prototype = {
 
 	
 		var MapLayers = GenerateMap(this.game, this.Map, 'AnteBoss', 'All_Tiles', 'Tiles');
-		// eugeneDial = new Dialogue(180,350,'eugeneDial',"Bien, si je ne me trompe pas, l'embarcadère de Charon \ndoit ce trouver au bout de ce cercle... \nEvitons d'attirer l'attention, j'aimerais eviter une effusion de \nsang inutile...");
-		// medusaDial = new Dialogue(160,350,'medusaDial',"Il parait qu'Eugène a disparu des bureaux. \nOn a ordre de le ramener si jamais on le vois");
-		// eugeneDial2 = new Dialogue(180,350,'eugeneDial',"HEIN QUOI ?! Déjà ? Il y'a balaine sous gravier \ns'ils savent déjà que je suis parti...\nSoyons discret");
-		// medusaDial.setVisible(false);
-		// eugeneDial.setVisible(false);
-		// eugeneDial2.setVisible(false);
-		// this.lastInput = this.game.time.now;
+		charonDial = new Dialogue(130,200,'charonDial',"HALTE LA !\nToutes âmes, morte ou vivante voulant passer doit payer\nson tribut!");
+		eugeneDial = new Dialogue(180,350,'eugeneDial',"Hey Charon vieille branche ! T'inquiète pas j'ai tout pré...\n*fouille fouille*\n Oh non... non non non non non");
+		charonDial2 = new Dialogue(130,200,'charonDial',"J'attend ton tribus Eugène !\nSi tu n'as pas de quoi payer... TU NE PASSERA PAS !!!");
+		eugeneDial2 = new Dialogue(180,350,'eugeneDial',"SUPER ton imitation, j'adore, mais serieux\nj'ai oublié ma bourse au bureau !\n*Et je me vois mal y retourner*");
+		charonDial3 = new Dialogue(130,200,'charonDial',"Tu connais les règles faucheusse,\n je ne te laisserais pas passer");
+		eugeneDial3 = new Dialogue(180,350,'eugeneDial',"Si tu le prend comme ça,\nJe n'ai pas d'autre choix que de forcer le passage a l'ancienne!");
+		charonDial4 = new Dialogue(130,200,'charonDial',"A nous deux !");
+		eugeneDial4 = new Dialogue(180,350,'eugeneDial',"Faux ! A nous " + Application.nbrSouls + 2);
+		
+		charonDial.setVisible(false);
+		eugeneDial.setVisible(false);
+		charonDial2.setVisible(false);
+		eugeneDial2.setVisible(false);
+		charonDial3.setVisible(false);
+		eugeneDial3.setVisible(false);
+		charonDial4.setVisible(false);
+		eugeneDial4.setVisible(false);
 
-		// this.DialArray = [eugeneDial,medusaDial,eugeneDial2];
-		// this.indexDial = 0;
+		this.lastInput = this.game.time.now;
 
-		// this.DialArray[this.indexDial].setVisible(true);
+		this.DialArray = [charonDial,eugeneDial,charonDial2,eugeneDial2,charonDial3,eugeneDial3,charonDial4,eugeneDial4];
+		this.indexDial = 0;
+
+		this.DialArray[this.indexDial].setVisible(true);
 				
 	},
 	update : function()
 	{
 		Application.Timer.Update();
-		// if (Application.Game.input.keyboard.isDown(Phaser.Keyboard.M) && ( this.lastInput + 500) < this.game.time.now )
-		// {
-		// 	console.log("ici")
-		// 	this.lastInput = this.game.time.now;
-		// 	this.DialArray[this.indexDial].setVisible(false);
-		// 	this.indexDial ++;
-		// 	if (this.indexDial < this.DialArray.length) 
-		// 	{
-		// 		this.DialArray[this.indexDial].setVisible(true);
-		// 	}
+		if (Application.Game.input.keyboard.isDown(Phaser.Keyboard.M) && ( this.lastInput + 500) < this.game.time.now )
+		{
+			console.log("ici")
+			this.lastInput = this.game.time.now;
+			this.DialArray[this.indexDial].setVisible(false);
+			this.indexDial ++;
+			if (this.indexDial < this.DialArray.length) 
+			{
+				this.DialArray[this.indexDial].setVisible(true);
+			}
 			
-		// }		
+		}		
 
 		
 	},
