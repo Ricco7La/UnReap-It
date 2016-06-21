3431f487e4c44b582df29e5e291614182d9b31e1
Kweena <michael.kweena.fossoul@gmail.com>
2016-06-21 13:36:05 +0200

    delete exit title

--- Assets/Scripts/Javascript/Scenes/Title.js
+++ Assets/Scripts/Javascript/Scenes/Title.js
@@ -35,30 +35,24 @@ Application.Title.prototype = {
 
 
 
-	    emitterStart = this.game.add.emitter(60,this.game.world.centerY,500,200);
+	    emitterStart = this.game.add.emitter(60,this.game.world.centerY + 50,500,200);
 		emitterStart.makeParticles('redParticles');
 	    emitterStart.setRotation(50, 50);
 	    emitterStart.setAlpha(0.7, 0.8);
 	    emitterStart.setScale(0.1, 0.1);
 	    emitterStart.gravity = 0;
 
-	    emitterCredit = this.game.add.emitter(60,this.game.world.centerY + 150,500,200);
+	    emitterCredit = this.game.add.emitter(60,this.game.world.centerY + 200,500,200);
 		emitterCredit.makeParticles('redParticles');
 	    emitterCredit.setRotation(50, 50);
 	    emitterCredit.setAlpha(0.7, 0.8);
 	    emitterCredit.setScale(0.1, 0.1);
 	    emitterCredit.gravity = 0;
 
-	    emitterExit = this.game.add.emitter(60,this.game.world.centerY + 200,500,200);
-		emitterExit.makeParticles('redParticles');
-	    emitterExit.setRotation(50, 50);
-	    emitterExit.setAlpha(0.7, 0.8);
-	    emitterExit.setScale(0.1, 0.1);
-	    emitterExit.gravity = 0;
 		
 
 
-	 	this.start = this.game.add.text(30, this.game.world.centerY - 10, "Start", { font: "20px Merriweather", fill: "#ff1105", align: "center" });
+	 	this.start = this.game.add.text(30, this.game.world.centerY + 40, "Start", { font: "20px Merriweather", fill: "#ff1105", align: "center" });
 		this.start.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
 		this.start.inputEnabled = true;
 		this.start.events.onInputOver.add(StartGame,this);
@@ -66,22 +60,16 @@ Application.Title.prototype = {
 			
 			
 
-		var load = this.game.add.text(30, this.game.world.centerY + 40, "Load", { font: "20px Merriweather", fill: "#808DC1", align: "center" });
+		var load = this.game.add.text(30, this.game.world.centerY + 90, "Load", { font: "20px Merriweather", fill: "#808DC1", align: "center" });
 			load.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
 
-		var option = this.game.add.text(30, this.game.world.centerY + 90, "Option", { font: "20px Merriweather", fill: "#808DC1", align: "center" });
+		var option = this.game.add.text(30, this.game.world.centerY + 140, "Option", { font: "20px Merriweather", fill: "#808DC1", align: "center" });
 			option.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
 
-		this.credit = this.game.add.text(30, this.game.world.centerY + 140, "Credit", { font: "20px Merriweather", fill: "#ff1105", align: "center" });
+		this.credit = this.game.add.text(30, this.game.world.centerY + 190, "Credit", { font: "20px Merriweather", fill: "#ff1105", align: "center" });
 		this.credit.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
 		this.credit.inputEnabled = true;
-		this.credit.events.onInputOver.add(StartCredit,this);
-
-
-		this.quit = this.game.add.text(30, this.game.world.centerY + 190, "Exit", { font: "20px Merriweather", fill: "#ff1105", align: "center" });
-		this.quit.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
-		
-        
+		this.credit.events.onInputOver.add(StartCredit,this);     
 	},
 
 	update : function()
@@ -94,12 +82,7 @@ Application.Title.prototype = {
 		if( !(( this.game.input.mousePointer.x |0) >= this.credit.x && (this.game.input.mousePointer.y |0) >= this.credit.y && (this.game.input.mousePointer.x |0) <= this.credit.x + this.credit.width && (this.game.input.mousePointer.y |0) <= this.credit.y + this.credit.height ))
 		{
 			emitterCredit.start(false, 300, 200);
-		}
-
-		if( !(( this.game.input.mousePointer.x |0) >= this.quit.x && (this.game.input.mousePointer.y |0) >= this.quit.y && (this.game.input.mousePointer.x |0) <= this.quit.x + this.quit.width && (this.game.input.mousePointer.y |0) <= this.quit.y + this.quit.height ))
-		{
-			emitterExit.start(false, 300, 200);
-		}			
+		}	
 	},
 
 	render : function()
