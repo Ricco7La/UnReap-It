4728e8dddaadf229b22caed0b474c3435804849f
Ricco7La <emeric.beauchot@gmail.com>
2016-06-22 16:06:47 +0200

    Escape Animation : Ninja

--- Assets/Scripts/Javascript/Game.js
+++ Assets/Scripts/Javascript/Game.js
@@ -5,7 +5,7 @@ var Application = {
 		height : 480
 	},
 	Game : null,
-	debugMode: true,
+	debugMode: false,
 	Layers: null,
 	Timer : null,
 	Juicy : null,
@@ -72,12 +72,13 @@ var Anim2 = function() {
 
     emitter.setRotation(0, 360);
     emitter.setAlpha(0.6);
-    emitter.setScale(0.5, 0, 0.5, 0, 2000);
+    emitter.setScale(0.3, 0, 0.3, 0, 2000);
     emitter.gravity = 0;
     emitter.setXSpeed(-75,75);
     emitter.setYSpeed(-75,75);
     emitter.makeParticles('SmokePuff');
 
+    Application.Game.camera.shake(0.03,350,true);
     emitter.start(false, 500, 10);
 
     Application.Layers.Player.visible = false;
@@ -94,35 +95,53 @@ var Anim2 = function() {
 }
 Application.EscapeAnimation.push(Anim2);
 
-var Anim3 = function() {
-	emitter = Application.Game.add.emitter(Application.Layers.Player.x, Application.Layers.Player.y, 100);
+var Anim3 = function() 
+{
 	Application.Layers.Player.canMove = false;
+	var wood = Application.Game.add.sprite(Application.Layers.Player.x,Application.Layers.Player.y,"Wood");
+	wood.visible = false;
+	wood.anchor.set(0.5);
+	wood.scale.set(0.65);
+	emitterLeaf = Application.Game.add.emitter(Application.Layers.Player.x, Application.Layers.Player.y, 5000);
 	Application.Layers.Player.body.damping = 1;
-	eugeneDial = new Dialogue(180,350,'eugeneDial',"");
+	Application.Layers.Player.animations.stop();
+	Application.Layers.Player.animations.frame = 0;
+	eugeneDial = new Dialogue(180,350,'eugeneDial',"Shiniton : Henge Kawarimi no Jutsu");
 	Application.Game.camera.unfollow();
 	
-	emitter.minParticleScale = 0.005;
-    emitter.maxParticleScale = 0.05;
+	emitterLeaf.minParticleScale = 0.005;
+    emitterLeaf.maxParticleScale = 0.05;
+    emitterLeaf.setRotation(0, 360);
+    emitterLeaf.setAlpha(0.6);
+    emitterLeaf.setScale(0.07, 0.05, 0.07, 0.05, 10000);
+    emitterLeaf.gravity = -25;
+    emitterLeaf.setXSpeed(-3,3);
+    emitterLeaf.setYSpeed(0,0);
+    emitterLeaf.makeParticles('Leaf');
 
-    emitter.setRotation(0, 360);
-    emitter.setAlpha(0.6);
-    emitter.setScale(0.5, 0, 0.5, 0, 2000);
-    emitter.gravity = -200;
-    emitter.setXSpeed(-3,3);
-    emitter.makeParticles('Leaf');
+    emitterLeaf.start(false, 2000, 1);
 
-    emitter.start(false, 500, 10);
+    var start = Application.Layers.Player.x - Application.Layers.Player.width;
+    var end = Application.Layers.Player.x + Application.Layers.Player.width;
 
-    Application.Layers.Player.visible = false;
+    emitterLeaf.emitY = Application.Layers.Player.y + Application.Layers.Player.height * 0.6;
+    emitterLeaf.emitX = start;
+    Application.Game.add.tween(emitterLeaf).to( { emitX: end }, 250, Phaser.Easing.Sinusoidal.InOut, true, 0, Number.MAX_VALUE, true);
+
     Application.Layers.Player.body.clearCollision();
 
     Application.Game.time.events.add(1500, function (argument) {
-    	emitter.on = false;
+    	emitterLeaf.on = false;
+    	Application.Layers.Player.visible = false;
+
     });
-    Application.Game.time.events.add(2250, function (argument) {
+    Application.Game.time.events.add(1600, function (argument) {
+    	wood.visible = true;
+
+    });
+    Application.Game.time.events.add(2500, function (argument) {
     	Application.Game.state.start(Application.lvl[Application.indexLevel], true);
     });
-    console.log(emitter);
 
 }
 Application.EscapeAnimation.push(Anim3);
