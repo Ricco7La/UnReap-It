cb5f788f8e5491352de9c0ba736df3521cc5bfd9
Kweena <michael.kweena.fossoul@gmail.com>
2016-06-23 14:56:19 +0200

    prepare the demo

--- Assets/Scripts/Javascript/Tools/Dialogue.js
+++ Assets/Scripts/Javascript/Tools/Dialogue.js
@@ -2,7 +2,7 @@ function Dialogue(_x,_y,_charaDial,_text)
 {
 	
 	var boxDialogue = Application.Game.add.image(0,0,_charaDial);
- 	boxDialogue.dialogue = Application.Game.add.text(_x,_y, _text, { font: "14px Lithos Pro", fill: "#000", align: "left" });
+ 	boxDialogue.dialogue = Application.Game.add.text(_x,_y, _text, { font: "13px Lithos Pro", fill: "#000", align: "left" });
  	boxDialogue.dialogue.setShadow(3, 3, 'rgba(0,0,0,0.2)', 5);
 	boxDialogue.fixedToCamera = true;
 	boxDialogue.dialogue.fixedToCamera = true;
