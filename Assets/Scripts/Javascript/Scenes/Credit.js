2d35b974a9ef19b4b1a2913bc61c176d022d09fc
Kweena <michael.kweena.fossoul@gmail.com>
2016-06-23 12:04:22 +0200

    change time credit

--- Assets/Scripts/Javascript/Scenes/Credit.js
+++ Assets/Scripts/Javascript/Scenes/Credit.js
@@ -4,7 +4,6 @@ Application.Credit = function()
 }
 
 
-
 Application.Credit.prototype = {
 
 	preload : function()
@@ -23,21 +22,16 @@ Application.Credit.prototype = {
 
 		setTimeout(function() 
 	    {
-	  //       if (_self.background.position.y <= -1700) 
-			// {
-				_self.tween = Application.Game.add.tween(_self.logo).to( { alpha: 1 }, 3000, Phaser.Easing.Linear.None,true);
-				_self.tween.onComplete.add(function()
-        	    {
-        	    	setTimeout(function()
-    	    		{
-    	    			console.log("fini")
-        	       		_self.state.start("Title");
-    	    		},3000)
-        	    })	
-			// }
-
+			_self.tween = Application.Game.add.tween(_self.logo).to( { alpha: 1 }, 3000, Phaser.Easing.Linear.None,true);
+			_self.tween.onComplete.add(function()
+    	    {
+    	    	setTimeout(function()
+	    		{
+	    			console.log("fini")
+    	       		_self.state.start("Title");
+	    		},3000)
+    	    })	
 	    },27000)
-	        
 	},
 
 	update : function()
@@ -45,19 +39,5 @@ Application.Credit.prototype = {
 		this.background.position.y -= 1;
 		
 	},
-
-	// end : function()
-	// {
-	// 	if (this.background.position.y <= -1700) 
-	// 	{
-	// 		this.tween = Application.Game.add.tween(this.logo).to( { alpha: 1 }, 1500, Phaser.Easing.Linear.None, true, 0, 1000, true);
-	// 		this.tween.onComplete.add(function()
- //            {
- //            	console.log("fini")
- //                this.state.start("Title");
- //            })	
-	// 	}
-	// },
-
 }
 
