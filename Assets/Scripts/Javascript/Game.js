a8e90c77012576c04e46c0f2c1b4e08e2ba852a2
Ricco7La <emeric.beauchot@gmail.com>
2016-06-27 16:10:37 +0200

    Boss can be killed

--- Assets/Scripts/Javascript/Game.js
+++ Assets/Scripts/Javascript/Game.js
@@ -11,7 +11,7 @@ var Application = {
 	Timer : null,
 	Juicy : null,
 	nbrSoulsBeforeLvl : 0,
-	nbrSouls : 8,
+	nbrSouls : 0,
 	lvl : ['Tuto','Ante1','Ante2','Ante3','AnteBoss','EndDemo'],
 	indexLevel : 0,
 	EscapeAnimation : [],
