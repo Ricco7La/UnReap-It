888d3de2d12e4e3420f46e9bfac351718c17453a
Azennhor <jd100989@gmail.com>
2016-06-27 12:01:22 +0200

    End Dial Area/ Tiled refont 2

---
+++ Assets/Scripts/Javascript/Tools/DialArea.js
@@ -0,0 +1,72 @@
+function DialArea( _game, _name, _x, _y, _width, _height)
+{
+	var type = "";
+    var _self = _game.add.sprite(_x, _y, type);
+    _self.name = _name;
+
+    _self.alreadyDid = false;
+
+    _self.width = _width;
+    _self.height = _height;
+   
+    _self.debug = Application.debugMode;
+
+    _self.lastInput = Application.Game.time.now;
+    _self.DialArray = [];
+    _self.indexDial = 0;
+
+    if(Application.debugMode)
+    {
+        var graphic = _game.add.graphics(0,0);
+        graphic.lineStyle(2, 0x0000ff, .4);
+        graphic.drawRect(_x, _y, _width, _height);
+    }
+
+    _self.update = function()
+    {
+        _self.LaunchDialogue();
+        _self.NextDialogue();
+    };
+
+    _self.CheckOverlap = function()
+    {
+        var dialBounds = _self.getBounds();
+        var playerBound =  Application.Layers.Player.getBounds();
+
+        return Phaser.Rectangle.intersects( dialBounds, playerBound);
+    };
+
+    _self.LaunchDialogue = function()
+    {
+        if(!_self.alreadyDid)
+        {
+            if(_self.CheckOverlap())
+            {
+                Application.Layers.Player.canMove = false;
+                Application.Layers.Player.body.setZeroVelocity();
+                _self.DialArray[_self.indexDial].setVisible(true);
+                _self.alreadyDid = true;
+            }
+        }
+    };
+
+    _self.NextDialogue = function()
+    {
+        if (Application.Game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) && ( _self.lastInput + 500) < Application.Game.time.now && _self.indexDial < _self.DialArray.length && _self.alreadyDid)
+        {
+            _self.lastInput = Application.Game.time.now;
+            _self.DialArray[_self.indexDial].setVisible(false);
+            _self.indexDial ++;
+            if (_self.indexDial < _self.DialArray.length) 
+            {
+                _self.DialArray[_self.indexDial].setVisible(true);
+            }
+            else
+            {
+                Application.Layers.Player.canMove = true;
+            }
+        }   
+    };
+
+    return _self;
+}
